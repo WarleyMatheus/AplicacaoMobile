339429ce602df041503edc6926077d66a1f9c3bb
mwarley735@gmail.com <mwarley735@gmail.com>
2024-05-09 20:43:06 -0300

    Project Init

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import { Text } from 'react-native';
+import Default from '../styles/Default';
+ 
+export default (props: any) => {
+  return (
+    <Text>  </Text> 
+  )
+}
